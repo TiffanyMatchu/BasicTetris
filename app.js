7676ce11558e22a024fea3859ba952beda1aef23
Tiffany Matthew <8907181+TiffanyMatchu@users.noreply.github.com>
2022-01-17 22:25:26 -0500

    Populated array based on html elements

--- app.js
+++ app.js
@@ -3,18 +3,24 @@ function makeGrid(l,w) {
     let gridDiv = document.getElementById("gameGrid");
     for (let i = 0; i < l; i++) {
       let row = document.createElement("div");
-      row.id = "row" + i;
+      let rowID = "row" + i;
+      row.id = rowID;
       row.className = "row"
       gridDiv.appendChild(row);
-      let currentRow = document.getElementById("row" + i);
+      let currentRow = document.getElementById(rowID);
       for (let j = 0; j < w; j++) {
         let col = document.createElement("div");
         col.id = "col" + j;
         col.className = "col";
         currentRow.appendChild(col);
       }
+      let rowString = "#" + rowID + " div";
+      gridArray.push(Array.from(document.querySelectorAll(rowString)));
     }
 }
-document.addEventListener('DOMContentLoaded', () => {    
-    makeGrid(25,30);
+document.addEventListener('DOMContentLoaded', () => {
+  makeGrid(25, 30);
+  console.log(gridArray);
 })
+
+
